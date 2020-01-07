5da1892373d12d0d2e11075894c32076e1a60be0
saijohiromu <gashjp1994@gmail.com>
2020-01-07 18:25:44 +0900

    Add files via upload

--- web/js/main.js
+++ web/js/main.js
@@ -1,37 +1,55 @@
 $(function () {
 
     console.log('start')
-    setTimeout(function () { GoRight('.gopher') }, 1000);
-
+    $('.gopher').offset({ top: 300, left: 30 });
+    // setTimeout(function () { GoGopher('.gopher', '300px') }, 1000);
+    JumpGopher('.gopher', true)
 });
 
-function GoRight(id) {
+function GoGopher(id, move, t) {
+    pos = $(id).offset();
     $(id).animate({
-        'right': '500px'
+        'left': (move + pos.left) + 'px'
     }, {
-            'duration': 3000,
+            'duration': t,
+            'delay': 100,
             'complete': function () {
 
-                // pos = $('.gopher').offset();
-                // console.log('pos ' + pos.left + ' window ' + $(window).width())
-                // if (pos.left > $(window).width()) {
-                //     return
-                // }
+                if (pos.left > $(window).width()) {
+                    console.log('end')
+                    return
+                }
 
-                console.log('go left')
-                setTimeout(function () { GoLeft(id) }, 1000);
+                JumpGopher(id, false);
             }
         });
 }
 
-function GoLeft(id) {
+function JumpGopher(id, flag) {
+    pos = $(id).offset();
+    abs = flag ? 1 : -1;
+    move = 70;
+    t = 1000;
     $(id).animate({
-        'left': '100px'
+        'left': (abs * move / 2 + pos.left) + 'px',
+        'top': (pos.top - 100) + 'px',
     }, {
-            'duration': 2000,
-            'complete': function () {
-                console.log('go right')
-                setTimeout(function () { GoRight(id) }, 1000);
-            }
-        });
+            'duration': t / 2,
+            'delay': 0,
+        }).animate({
+            'left': (abs * move + pos.left) + 'px',
+            'top': (pos.top - 56) + 'px',
+        }, {
+                'duration': t / 2,
+                'delay': 50,
+                'complete': function () {
+
+                    if (pos.left > $(window).width()) {
+                        console.log('end')
+                        return
+                    }
+
+                    GoGopher(id, 200, 2000);
+                }
+            });
 }
